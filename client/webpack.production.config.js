c643a29bfa50ccbef687251ddc12624cce5dcd9d
BaharDev <bahar.cranb@gmail.com>
2020-09-07 20:01:48 -0400

    preparing for deployment

--- client/webpack.production.config.js
+++ client/webpack.production.config.js
@@ -5,7 +5,7 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = () => ({
   mode: "production",
   output: {
-    path: path.resolve(__dirname, "./build/"),
+    path: path.resolve(__dirname, "./build"),
     filename: "js/[name].js"
   },
   optimization: {
