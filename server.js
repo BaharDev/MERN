6b41f26d9fe2d5005c9db26564547e9f1d9f5963
BaharDev <bahar.cranb@gmail.com>
2020-09-07 22:01:53 -0400

    preparing for deployment

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.get("*", (res, req) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`server started on port ${port}`)});
